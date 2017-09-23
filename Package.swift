d11eeef09c066f03d609acd07076e963d5340aa2
Tim <0xtimc@gmail.com>
2017-09-23 19:21:16 +0100

    Add Swift 3 compatible Pacakge.swift

--- Package.swift
+++ Package.swift
@@ -1,17 +1,8 @@
-// swift-tools-version:4.0
-
 import PackageDescription
 
 let package = Package(
     name: "LeafErrorMiddleware",
-    products: [
-        .library(name: "LeafErrorMiddleware", targets: ["LeafErrorMiddleware"]),
-    ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "2.1.0"),
-    ],
-    targets: [
-        .target(name: "LeafErrorMiddleware", dependencies: ["Vapor"]),
-        .testTarget(name: "LeafErrorMiddlewareTests", dependencies: ["LeafErrorMiddleware"]),
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
     ]
 )
