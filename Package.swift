eefd7392c1bca156b4e2c32503364aec918c232e
Nathan <bre7@users.noreply.github.com>
2018-03-28 11:13:12 -0300

    Updated to Swift 4

--- Package.swift
+++ Package.swift
@@ -1,8 +1,16 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
     name: "LeafErrorMiddleware",
+    products: [
+        .library(name: "LeafErrorMiddleware", targets: ["LeafErrorMiddleware"]),
+    ],
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", .branch("master") ),
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0-rc"),
+    ],
+    targets: [
+        .target(name: "LeafErrorMiddleware", dependencies: ["Vapor"]),
+        .testTarget(name: "LeafErrorMiddlewareTests", dependencies: ["LeafErrorMiddleware"]),
     ]
 )
