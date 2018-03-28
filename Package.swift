2f08e6cd57c59f9feb4c79cda50c9601b4d55932
Nathan <bre7@users.noreply.github.com>
2018-03-27 21:43:09 -0300

    Use master branch

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "LeafErrorMiddleware",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
+        .Package(url: "https://github.com/vapor/vapor.git", .branch("master") ),
     ]
 )
