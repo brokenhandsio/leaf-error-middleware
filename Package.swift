067441dc6a01ed175fb38f6cdac38e0096f76e60
Tim <0xtimc@gmail.com>
2018-06-02 14:37:29 +0100

    Update manifest

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         .library(name: "LeafErrorMiddleware", targets: ["LeafErrorMiddleware"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0-rc"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
     ],
     targets: [
         .target(name: "LeafErrorMiddleware", dependencies: ["Vapor"]),
